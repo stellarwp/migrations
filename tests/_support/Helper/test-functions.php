76336aa8bb6ce13c59a1fa6ea6e2a7a65ea8cae8
Dimitrios Pantazis <dpanta94@gmail.com>
2025-12-02 17:56:29 +0200

    tests: amend bootstrap with table dropping and shepherd sync execution

--- tests/_support/Helper/test-functions.php
+++ tests/_support/Helper/test-functions.php
@@ -3,7 +3,54 @@ use StellarWP\Migrations\Tests\Container;
 use StellarWP\ContainerContract\ContainerInterface;
 use StellarWP\DB\DB;
 use StellarWP\Migrations\Config;
+use StellarWP\Shepherd\Config as Shepherd_Config;
 use StellarWP\Migrations\Provider;
+use StellarWP\Migrations\Tasks\Execute;
+use StellarWP\Migrations\Tables\Migration_Events;
+use StellarWP\Shepherd\Tables\Utility\Safe_Dynamic_Prefix;
+/**
+ * Drop the tables before and after the suite.
+ *
+ * @return void
+ */
+function tests_migrations_drop_tables() {
+	$container           = tests_migrations_get_container();
+	$safe_dynamic_prefix = $container->get( Safe_Dynamic_Prefix::class );
+
+	$tables        = [];
+	$table_classes = [
+		Migration_Events::class,
+	];
+
+	$longest_table_name = $safe_dynamic_prefix->get_longest_table_name( $table_classes );
+
+	foreach ( $table_classes as $table_class ) {
+		$tables[] = sprintf( $table_class::raw_base_table_name(), $safe_dynamic_prefix->get( $longest_table_name ) );
+	}
+
+	foreach ( $tables as $table ) {
+		DB::query(
+			DB::prepare( 'DROP TABLE IF EXISTS %i', DB::prefix( $table ) )
+		);
+	}
+}
+
+/**
+ * Raises the auto increment for the tables.
+ *
+ * @return void
+ */
+function tests_migrations_raise_auto_increment(): void {
+	$tables = [
+		Migration_Events::base_table_name(),
+	];
+
+	foreach ( $tables as $offset => $table ) {
+		DB::query(
+			DB::prepare( 'ALTER TABLE %i AUTO_INCREMENT = %d', DB::prefix( $table ), 728365 + ( 1 + (int) $offset * 3 ) )
+		);
+	}
+}
 
 /**
  * Resets the config.
@@ -11,7 +58,11 @@ use StellarWP\Migrations\Provider;
  * @return void
  */
 function tests_migrations_reset_config(): void {
+	Shepherd_Config::reset();
 	Config::reset();
+	Shepherd_Config::set_hook_prefix( tests_migrations_get_hook_prefix() );
+	Shepherd_Config::set_container( tests_migrations_get_container() );
+	Shepherd_Config::set_max_table_name_length( 64 );
 	Config::set_hook_prefix( tests_migrations_get_hook_prefix() );
 	Config::set_container( tests_migrations_get_container() );
 }
@@ -70,6 +121,7 @@ function tests_migrations_fake_transactions_disable() {
  */
 function tests_migrations_common_bootstrap(): void {
 	tests_migrations_reset_config();
+	tests_migrations_drop_tables();
 	tests_migrations_fake_transactions_enable();
 
 	$container = Config::get_container();
@@ -79,4 +131,82 @@ function tests_migrations_common_bootstrap(): void {
 	// Bootstrap and register Migrations.
 	$container->singleton( Provider::class );
 	$container->get( Provider::class )->register();
+
+	// Raise the auto increment for the tables.
+	tests_migrations_raise_auto_increment();
+
+	// We don't test shepherd functionality here, we expect it to work as expected.
+	tests_migrations_shepherd_sync_enable();
+
+	// Drop the tables after the tests are done.
+	tests_add_filter(
+		'shutdown',
+		'tests_migrations_drop_tables',
+		10000
+	);
+}
+
+/**
+ * Data for the migrations execute calls.
+ *
+ * @var array
+ */
+$GLOBALS['stellarwp_migrations_execute_calls_data'] = [];
+
+/**
+ * Callback for the migrations execute calls.
+ *
+ * @var callable
+ */
+$GLOBALS['stellarwp_migrations_execute_callback'] = function ( $handler, $task ) {
+	if ( ! $task instanceof Execute ) {
+		return $handler;
+	}
+
+	return function () use ( $task ) {
+		global $stellarwp_migrations_execute_calls_data;
+		$stellarwp_migrations_execute_calls_data[] = $task;
+
+		$task->process();
+	};
+};
+
+/**
+ * Get the calls data.
+ *
+ * @return array
+ */
+function tests_migrations_get_calls_data(): array {
+	global $stellarwp_migrations_execute_calls_data;
+	return $stellarwp_migrations_execute_calls_data;
+}
+
+/**
+ * Clear the calls data.
+ *
+ * @return void
+ */
+function tests_migrations_clear_calls_data(): void {
+	global $stellarwp_migrations_execute_calls_data;
+	$stellarwp_migrations_execute_calls_data = [];
+}
+
+/**
+ * Enables the migrations execute calls.
+ *
+ * @return void
+ */
+function tests_migrations_shepherd_sync_enable() {
+	global $stellarwp_migrations_execute_callback;
+	add_filter( 'shepherd_tec_dispatch_handler', $stellarwp_migrations_execute_callback, 10, 2 );
+}
+
+/**
+ * Disables the migrations execute calls.
+ *
+ * @return void
+ */
+function tests_migrations_shepherd_sync_disable() {
+	global $stellarwp_migrations_execute_callback;
+	remove_filter( 'shepherd_tec_dispatch_handler', $stellarwp_migrations_execute_callback );
 }
