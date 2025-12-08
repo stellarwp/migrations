1d806b8a013a5b4fabd9c92c4639eb6810360bd0
Dimitrios Pantazis <dpanta94@gmail.com>
2025-12-08 15:40:17 +0200

    tests: uncomment valid test cases

--- tests/wpunit/Student_Meta_Migration_Test.php
+++ tests/wpunit/Student_Meta_Migration_Test.php
@@ -24,59 +24,59 @@ class Student_Meta_Migration_Test extends WPTestCase {
 	}
 
 	public function dummy_data_provider(): Generator {
-		// yield 'no data to migrate' => [
-		// 	function (): array {
-		// 		return [];
-		// 	}
-		// ];
+		yield 'no data to migrate' => [
+			function (): array {
+				return [];
+			}
+		];
 
-		// yield 'single post course access from' => [
-		// 	function (): array {
-		// 		$uid = self::factory()->user->create( [ 'role' => 'contributor' ] );
+		yield 'single post course access from' => [
+			function (): array {
+				$uid = self::factory()->user->create( [ 'role' => 'contributor' ] );
 
-		// 		add_user_meta( $uid, 'user_id', $uid );
-		// 		add_user_meta( $uid, 'course_1_access_from', '2025-01-01' );
+				add_user_meta( $uid, 'user_id', $uid );
+				add_user_meta( $uid, 'course_1_access_from', '2025-01-01' );
 
-		// 		return [ $uid ];
-		// 	}
-		// ];
+				return [ $uid ];
+			}
+		];
 
-		// yield 'single post group access from' => [
-		// 	function (): array {
-		// 		$uid = self::factory()->user->create( [ 'role' => 'contributor' ] );
+		yield 'single post group access from' => [
+			function (): array {
+				$uid = self::factory()->user->create( [ 'role' => 'contributor' ] );
 
-		// 		add_user_meta( $uid, 'user_id', $uid );
-		// 		add_user_meta( $uid, 'group_1_access_from', '2025-01-01' );
+				add_user_meta( $uid, 'user_id', $uid );
+				add_user_meta( $uid, 'group_1_access_from', '2025-01-01' );
 
-		// 		return [ $uid ];
-		// 	}
-		// ];
+				return [ $uid ];
+			}
+		];
 
-		// yield 'multiple posts course access from' => [
-		// 	function (): array {
-		// 		$uids = self::factory()->user->create_many( 5, [ 'role' => 'contributor' ] );
+		yield 'multiple posts course access from' => [
+			function (): array {
+				$uids = self::factory()->user->create_many( 5, [ 'role' => 'contributor' ] );
 
-		// 		foreach ( $uids as $uid ) {
-		// 			add_user_meta( $uid, 'user_id', $uid );
-		// 			add_user_meta( $uid, 'course_1_access_from', '2025-01-01' );
-		// 		}
+				foreach ( $uids as $uid ) {
+					add_user_meta( $uid, 'user_id', $uid );
+					add_user_meta( $uid, 'course_1_access_from', '2025-01-01' );
+				}
 
-		// 		return $uids;
-		// 	}
-		// ];
+				return $uids;
+			}
+		];
 
-		// yield 'multiple posts group access from' => [
-		// 	function (): array {
-		// 		$uids = self::factory()->user->create_many( 5, [ 'role' => 'contributor' ] );
+		yield 'multiple posts group access from' => [
+			function (): array {
+				$uids = self::factory()->user->create_many( 5, [ 'role' => 'contributor' ] );
 
-		// 		foreach ( $uids as $uid ) {
-		// 			add_user_meta( $uid, 'user_id', $uid );
-		// 			add_user_meta( $uid, 'group_1_access_from', '2025-01-01' );
-		// 		}
+				foreach ( $uids as $uid ) {
+					add_user_meta( $uid, 'user_id', $uid );
+					add_user_meta( $uid, 'group_1_access_from', '2025-01-01' );
+				}
 
-		// 		return $uids;
-		// 	}
-		// ];
+				return $uids;
+			}
+		];
 
 		yield 'multiple posts course and group access from' => [
 			function (): array {
