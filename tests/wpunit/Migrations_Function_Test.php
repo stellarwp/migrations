64058e586c819b55355999b25c9326930cd63cf7
Dimitrios Pantazis <dpanta94@gmail.com>
2026-01-23 18:01:35 +0200

    tests: improve usage of set/get static with uopz

--- tests/wpunit/Migrations_Function_Test.php
+++ tests/wpunit/Migrations_Function_Test.php
@@ -11,15 +11,25 @@ use function StellarWP\Migrations\migrations;
 class Migrations_Function_Test extends WPTestCase {
 	use With_Uopz;
 
+	private $stored_static_value = null;
+
 	/**
 	 * Reset the static variable in the migrations function before each test.
 	 *
 	 * @before
 	 */
 	public function reset_migrations_static(): void {
-		if ( function_exists( 'uopz_set_static' ) ) {
-			uopz_set_static( 'StellarWP\\Migrations\\migrations', [ 'migrations' => null ] );
-		}
+		$this->stored_static_value = uopz_get_static( 'StellarWP\\Migrations\\migrations' );
+		uopz_set_static( 'StellarWP\\Migrations\\migrations', [ 'migrations' => null ] );
+	}
+
+	/**
+	 * Reset the static variable after each test.
+	 *
+	 * @after
+	 */
+	public function cleanup_migrations_static(): void {
+		uopz_set_static( 'StellarWP\\Migrations\\migrations', $this->stored_static_value );
 	}
 
 	/**
@@ -46,16 +56,5 @@ class Migrations_Function_Test extends WPTestCase {
 		$registry = $provider->get_registry();
 
 		$this->assertInstanceOf( Registry::class, $registry );
-	}
-
-	/**
-	 * Reset the static variable after each test.
-	 *
-	 * @after
-	 */
-	public function cleanup_migrations_static(): void {
-		if ( function_exists( 'uopz_set_static' ) ) {
-			uopz_set_static( 'StellarWP\\Migrations\\migrations', [ 'migrations' => null ] );
-		}
 	}
 }
