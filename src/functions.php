449132fa3b2f2125b7e6e280c99b29519d241594
Dimitrios Pantazis <dpanta94@gmail.com>
2026-01-23 17:23:07 +0200

    fix: simplified migrations function

--- src/functions.php
+++ src/functions.php
@@ -9,22 +9,14 @@ declare( strict_types=1 );
 
 namespace StellarWP\Migrations;
 
-use RuntimeException;
-
 /**
  * Get the Migrations's Provider instance.
  *
  * @since 0.0.1
  *
  * @return Provider The Migrations's Provider.
- *
- * @throws RuntimeException If Migrations is not registered.
  */
 function migrations(): Provider {
-	if ( ! Provider::is_registered() ) {
-		throw new RuntimeException( 'Migrations library is not yet registered!' );
-	}
-
 	/** @var Provider|null $migrations */
 	static $migrations = null;
 
